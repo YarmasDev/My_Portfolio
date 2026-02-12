45b0e2a29c9d19d33453dff46234de4d2754423c
YarmasDev <nickolasyarmas@gmail.com>
2026-02-12 14:53:46 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/My_Portfolio/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
